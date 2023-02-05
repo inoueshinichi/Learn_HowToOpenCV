0ee6254d0be3cdc6a5142085b3a283b06552b268
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-02-05 23:49:44 +0900

    [Update] Binalization

--- binalization/binarize.cpp
+++ binalization/binarize.cpp
@@ -8,76 +8,71 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <random>
+#include <test_utils.hpp>
 #include <opencv2/opencv.hpp>
-using namespace std;
-using namespace cv;
+#include <string>
+#include <iostream>
+
+#define CV_IMSHOW(mat) cv::imshow(#mat, mat);
 
 int main(int argc, char** argv)
 {
     try
     {
-        if (argc < 2)
-            throw ("few parameters.");
+        // テスト画像
+        std::string test_file = GetTestData("nekosan.jpg");
+        std::cout << "Test file path: " << test_file << std::endl;
 
         // 画像読み込み
-        Mat img_src;
-        img_src = imread(argv[1], IMREAD_GRAYSCALE);
+        cv::Mat img_src;
+        img_src = cv::imread(test_file, cv::IMREAD_GRAYSCALE);
         if (img_src.empty())
-            throw ("failed open file.");
+            throw("failed open file.");
+        CV_IMSHOW(img_src)
 
         // 画像準備
-        Mat img_dst1, img_dst2, img_dst3, img_dst4, img_dst5, img_dst6;
-        img_src.copyTo(img_dst1);
-        img_src.copyTo(img_dst2);
-        img_src.copyTo(img_dst3);
-        img_src.copyTo(img_dst4);
-        img_src.copyTo(img_dst5);
-        img_src.copyTo(img_dst6);
-
+        cv::Mat img_bin, img_bin_inv, img_bin_trunc, img_bin_tozero, img_bin_tozero_inv, img_masked;
+        
         // 通常の２値化処理
         int thresh = 100;
-        threshold(img_src, img_dst1, thresh, 255, THRESH_BINARY);
-        imshow("img_dst1", img_dst1);
+        cv::threshold(img_src, img_bin, thresh, 255, cv::THRESH_BINARY);
+        CV_IMSHOW(img_bin)
 
         // 反転２値化処理
-        threshold(img_src, img_dst2, thresh, 255, THRESH_BINARY_INV);
-        imshow("img_dst2", img_dst2);
+        cv::threshold(img_src, img_bin_inv, thresh, 255, cv::THRESH_BINARY_INV);
+        CV_IMSHOW(img_bin_inv)
         
         // 入力画像の画素値がしきい値より大きい場合はしきい値、
         // それ以外の場合は入力画像のまま。２値化処理
-        threshold(img_src, img_dst3, thresh, 255, THRESH_TRUNC);
-        imshow("img_dst3", img_dst3);
+        cv::threshold(img_src, img_bin_trunc, thresh, 255, cv::THRESH_TRUNC);
+        CV_IMSHOW(img_bin_trunc)
 
         // 入力画像の画素値がしきい値より大きい場合は
         // 入力画像のまま.それ以外は０となる
-        threshold(img_src, img_dst4, thresh, 255, THRESH_TOZERO);
-        imshow("img_dst4", img_dst4);
+        cv::threshold(img_src, img_bin_tozero, thresh, 255, cv::THRESH_TOZERO);
+        CV_IMSHOW(img_bin_tozero)
 
         // 入力画像の画素値がしきい値より大きい場合は０.
         // それ以外は入力画像のままとなる.
-        threshold(img_src, img_dst5, thresh, 255, THRESH_TOZERO_INV);
-        imshow("img_dst5", img_dst5);
+        cv::threshold(img_src, img_bin_tozero_inv, thresh, 255, cv::THRESH_TOZERO_INV);
+        CV_IMSHOW(img_bin_tozero_inv)
 
         // マスク処理
-        Mat img_mask = Mat::zeros(img_src.rows, img_src.cols, CV_8U);
+        cv::Mat img_mask = cv::Mat::zeros(img_src.rows, img_src.cols, CV_8U);
         for (int y = (int)(img_mask.rows*0.3); y < (int)(img_mask.rows*0.5); y++) {
             for (int x = (int)(img_mask.cols*0.3); x < (int)(img_mask.cols*0.5); x++) {
                 img_mask.data[y * img_mask.step + x] = 255;
             }
         }
-        imshow("img_mask", img_mask);
-        img_src.copyTo(img_dst6, img_mask);
-        imshow("img_dst6", img_dst6);
+        CV_IMSHOW(img_mask)
+        img_src.copyTo(img_masked, img_mask);
+        CV_IMSHOW(img_masked)
 
-        waitKey(0);
+        cv::waitKey(0);
     }
     catch (const char* str)
     {
-        cerr << str << endl;
+        std::cerr << str << std::endl;
     }
     return 0;
 }
